a012cad0d9173e920e15da7daa97d299c42d5fa9
Saud215 <mohdsaud1523@gmail.com>
2025-09-06 23:16:16 +0530

    added the eks moduleand its implementation

--- modules/eks/variables.tf
+++ modules/eks/variables.tf
@@ -1,28 +1,6 @@
-variable "vpc_cidr" {
-
-    description = "The cidr block value of our VPC"
-    type = string
-}
-variable "public_subnet_cidrs" {
-
-    description = "The cidr blocks for our public subnets inside our VPC"
-    type = list(string)
-}
-variable "private_subnet_cidrs" {
-
-    description = "The cidr blocks for our private subnets inside our VPC"
-    type = list(string)
-}
-variable "availability_zones" {
-
-    description = "The availability zones for our subnets"
-    type = list(string)
-}
-
 variable "cluster_name" {
-
-    description = "The name of our EKS cluster"
-    type = string 
+  description = "Name of the EKS cluster"
+  type        = string
 }
 
 variable "cluster_version" {
